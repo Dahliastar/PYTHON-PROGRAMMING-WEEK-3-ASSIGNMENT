070636234f13c475ba6e8eeedb22e8bc817b6135
Michelle Ndunge <157975072+Dahliastar@users.noreply.github.com>
2025-10-02 15:25:15 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -5,8 +5,10 @@ def calculate_discount(price, discount_percent):
     if discount_percent >= 20:
          discount = price * (discount_percent / 100) 
          final_price = price - discount
+         print("Discounted price:",final_price)
          return final_price
     else: 
-        print(price)
+        print("No discount applied. Price remains:",price)
+        return price
 
 calculate_discount(price, discount_percent)
